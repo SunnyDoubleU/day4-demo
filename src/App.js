11f2cc8f292cbcf590be0afc446af65461866751
Sunny Wong <sunnywong18@gmail.com>
2019-10-17 10:58:20 +0200

    w8 d4 routing inclass practice

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Link, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="AppNav">
+        <Link className="AppA" to="/Home">Home</Link>
+        <Link className="AppA" to="/About">About</Link>
+        <Link className="AppA" to="/Contact">Contact</Link>
+      </div>
+
+      <div className="App">
+        {/* <Route path="/Home">
+        <Home />
+      </Route>
+      <Route path="/About">
+        <About />
+      </Route>
+      <Route path="/Contact">
+        <Contact />
+      </Route> */}
+
+        <Route path="/Home" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </div>
     </div>
   );
 }
